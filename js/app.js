372eb38d4d5f3f55a6839321111bd028974e1c65
afzalhossenhasib <ahhasib47@gmail.com>
2022-03-01 04:23:51 +0600

    detail added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 
-
 const loadPhoneData = () => {
     const searchField = document.getElementById("input-field");
     const searchPhone = searchField.value;
@@ -10,9 +9,9 @@ const loadPhoneData = () => {
         `;
         fetch(url)
         .then((res) => res.json())
-        .then((data) => displayPhone(data.data));
-        
+        .then((data) => displayPhone(data.data));  
 };
+
 
 const displayPhone = (phones) => {
     console.log (phones);
@@ -22,16 +21,47 @@ const displayPhone = (phones) => {
              div.classList.add('col');
              div.innerHTML = `
              <div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <h5 class="card-title">${phone.phone_name}</h5>
+                  <p class="card-text">${phone.brand}</p>
+                  <button onclick="loadDetails ('${phone.slug}')" class ="btn btn-info"> Details</button>
                 </div>
               </div>
              `
              searchResult.appendChild(div);
-            //  const {phone_name} = phone
-            //  console.log (phone_name);
          });
       
+}
+
+
+const loadDetails = (phoneId) => {
+    const url = `
+    https://openapi.programming-hero.com/api/phone/${phoneId}
+    `;
+    fetch (url)
+    .then (res => res.json())
+    .then (data => displayDetails (data.data));
+}
+
+const displayDetails = (detail) => {
+    console.log(detail);
+    const div = document.createElement('div');
+    console.log(detail.mainFeatures)
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card h-100">
+       <img src="${detail.image}" class="card-img-top img-fluid" alt="...">
+       <div class="card-body">
+         <h5 class="card-title">${detail.name}</h5>
+         <p class="card-text">Chipset: ${detail.mainFeatures.chipSet}</p>
+         <p class="card-text">Displaysize: ${detail.mainFeatures.displaySize}</p>
+         <p class="card-text">Memory: ${detail.mainFeatures.memory}</p>
+         <p class="card-text">Storage: ${detail.mainFeatures.storage}</p>
+         <p class="card-text">Sensors: ${detail.sensors}</p>
+         <p class="card-text">Releasedate: ${detail.releaseDate}</p>
+       </div>
+     </div>
+    `
+    document.getElementById('detail-container').appendChild (div);
 }
