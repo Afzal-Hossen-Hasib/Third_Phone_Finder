1e50d0f15e8844aa5239bc6a3d733cb644980d3a
afzalhossenhasib <ahhasib47@gmail.com>
2022-03-01 14:32:09 +0600

    sensor added

--- js/app.js
+++ js/app.js
@@ -5,14 +5,23 @@
 const loadPhoneData = () => {
     const searchField = document.getElementById("input-field");
     const searchPhone = searchField.value;
+
+    // Clear Data 
     searchField.value = '';
 
-        const url = `
-        https://openapi.programming-hero.com/api/phones?search=${searchPhone}
-        `;
-        fetch(url)
-        .then((res) => res.json())
-        .then((data) => displayPhone(data.data));  
+    if (searchPhone == '') {
+      console.log ('phone nai')
+    }
+
+    else {
+      // Load Data 
+      const url = `
+      https://openapi.programming-hero.com/api/phones?search=${searchPhone}
+      `;
+      fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayPhone(data.data)); 
+    } 
 };
 
 // -------------------------------------------------
@@ -21,21 +30,27 @@ const loadPhoneData = () => {
 const displayPhone = (phones) => {
     console.log (phones);
         const searchResult = document.getElementById ('search-result');
-         phones.forEach(phone => {
-             const div = document.createElement('div');
-             div.classList.add('col');
-             div.innerHTML = `
-             <div class="card h-100 phone-container">
-                <img src="${phone.image}" class="card-img-top w-50" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${phone.phone_name}</h5>
-                  <p class="card-text">${phone.brand}</p>
-                  <button onclick="loadDetails ('${phone.slug}')" class ="btn btn-info"> Details</button>
-                </div>
-              </div>
-             `
-             searchResult.appendChild(div);
-         });
+        searchResult.textContent = '';
+        if (phones.length == 0) {
+          console.log('pai nai kisu')
+        }
+        else {
+          phones.forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100 phone-container">
+               <img src="${phone.image}" class="card-img-top w-50" alt="...">
+               <div class="card-body">
+                 <h5 class="card-title">Name : ${phone.phone_name}</h5>
+                 <p class="card-text">Brand : ${phone.brand}</p>
+                 <button onclick="loadDetails ('${phone.slug}')" class ="btn btn-info"> Details</button>
+               </div>
+             </div>
+            `
+            searchResult.appendChild(div);
+        });
+         }
 }
 
 // ----------------------------------------
@@ -55,7 +70,8 @@ const loadDetails = (phoneId) => {
 // --------------------------------------------
 const displayDetails = (detail) => {
     console.log(detail);
-    const detailInformation = document.getElementById('detail-container');
+    const phoneDetail = document.getElementById('detail-container');
+    phoneDetail.textContent = '';
     const div = document.createElement('div');
     console.log(detail.mainFeatures)
     div.classList.add('col');
@@ -63,16 +79,27 @@ const displayDetails = (detail) => {
     <div class="card h-100 phone-container">
        <img src="${detail.image}" class="card-img-top w-25" alt="...">
        <div class="card-body">
+         <h5 class="card-title">${detail.brand}</h5>
          <h5 class="card-title">${detail.name}</h5>
-         <h5 class="card-title">${detail.name}</h5>
+         <h2> Main Features </h2>
          <p class="card-text"> <span class = "detail-title"> Chipset: </span> ${detail.mainFeatures.chipSet}</p>
          <p class="card-text"> <span class = "detail-title"> Displaysize: </span> ${detail.mainFeatures.displaySize}</p>
          <p class="card-text"> <span class = "detail-title"> Memory: </span> ${detail.mainFeatures.memory}</p>
          <p class="card-text"> <span class = "detail-title"> Storage: </span> ${detail.mainFeatures.storage}</p>
-         <p class="card-text"> <span class = "detail-title"> Sensors: </span> ${detail.sensors}</p>
+
+
+         ${detail.mainFeatures?.sensors.forEach((sensor) => {
+          //  const sensorText = document.createElement ('p');
+          //  sensorText.innerText = `${sensor}`
+          `<p>${sensor}</p>`
+         }) } 
+        
          <p class="card-text"> <span class = "detail-title"> Releasedate: </span> ${detail.releaseDate}</p>
+         
        </div>
      </div>
     `
-    detailInformation.appendChild (div);
+    phoneDetail.appendChild (div);
+
+    // console.log(detail.mainFeatures.sensors);
 }
